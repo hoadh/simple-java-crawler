fbf7c4e839324e8204eca4adcac5af13ffb10c59
Hoa <danghuyhoa@gmail.com>
2020-07-24 14:00:09 +0700

    Remove a crawler

--- src/com/codegym/MySimpleCrawler.java
+++ src/com/codegym/MySimpleCrawler.java
@@ -9,11 +9,11 @@ public class MySimpleCrawler {
 
     public static void main(String[] args) throws Exception {
         IHttpService urlGetRequest = new URLGetRequest();
-        Crawler[] crawlers = new Crawler[2];
+        Crawler[] crawlers = new Crawler[1];
         crawlers[0] = new BatdongsanCrawler(urlGetRequest);
 
         for (Crawler crawler: crawlers) {
-            crawler.inspect();
+            if (crawler != null) crawler.inspect();
         }
     }
 }
